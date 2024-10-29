e559d648a13ed450a7d95756b88d5adda5b1ffa7
mwinkens <104770531+mwinkens@users.noreply.github.com>
2024-10-29 13:40:03 +0100

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -86,6 +86,16 @@ class proxysql::params {
           'server' => 'keyserver.ubuntu.com',
         },
       }
+      $repo27             = {
+        comment  => 'ProxySQL 2.7.x APT repository',
+        location => "http://repo.proxysql.com/ProxySQL/proxysql-2.7.x/${facts['os']['distro']['codename']}/",
+        release  => './',
+        repos    => '',
+        key      => {
+          'id'     => '1448BF693CA600C799EB935804A562FB79953B49',
+          'server' => 'keyserver.ubuntu.com',
+        },
+      }
     }
     'RedHat': {
       $package_provider = 'rpm'
